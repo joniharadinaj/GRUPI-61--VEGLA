6141fb2bb2d02fea8b0a47e4cd373b5683e22141
joniharadinaj <joni.haradinaj@student.uni-pr.edu>
2026-01-03 01:17:03 +0100

    Update projekti.cpp

--- projekti.cpp
+++ projekti.cpp
@@ -5,9 +5,12 @@ using namespace std;
 int power(int base, int exp) {
     return pow(base, exp);
 }
+int modulo(int a, int b) {
+    return a % b;
+}
 int main(){
 
     cout << "Power: " << power(2, 3) << endl;
-
+    cout << "Modulo: " << modulo(10, 3) << endl;
     return 0;
 }

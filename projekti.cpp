6998197a3ed3baf103dd7aee0c1f2ddef45c4e18
Erza Rexhepi <erza.rexhepi3@student.uni-pr.edu>
2026-01-03 01:46:35 +0100

    Update projekti.cpp
    Added multiply function

--- projekti.cpp
+++ projekti.cpp
@@ -14,11 +14,18 @@ int maxNumber(int a, int b) {
 int minNumber(int a, int b) {
     return (a < b) ? a : b;
 }
+
+int multiply(int a, int b) {
+    return a * b;
+}
 int main(){
+
 
     cout << "Power: " << power(2, 3) << endl;
     cout << "Modulo: " << modulo(10, 3) << endl;
     cout << "Max: " << maxNumber(10, 5) << endl;
     cout << "Min: " << minNumber(10, 5) << endl;
+    int result = multiply(5, 3); 
+    cout << "The result is: " << result << endl;
     return 0;
 }

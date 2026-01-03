299e1102fd5787ceb2d68d6e4e570cc55a01560b
joniharadinaj <joni.haradinaj@student.uni-pr.edu>
2026-01-03 01:14:17 +0100

    Update projekti.cpp

--- projekti.cpp
+++ projekti.cpp
@@ -2,11 +2,12 @@
 #include <cmath>
 using namespace std;
 
-
-
-
+int power(int base, int exp) {
+    return pow(base, exp);
+}
 int main(){
 
+    cout << "Power: " << power(2, 3) << endl;
 
     return 0;
 }
